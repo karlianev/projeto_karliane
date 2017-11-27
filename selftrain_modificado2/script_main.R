9aa29338634ca31106c5396363a4930d48e9df38
karliane Vale <karlianev@hotmail.com>
2017-11-27 09:34:49 -0200

    nenhuma alteracao importante, somente testes

--- selftrain_modificado2/script_main.R
+++ selftrain_modificado2/script_main.R
@@ -28,8 +28,8 @@ source('C:/local_R/projeto_karliane/scripts_comuns/configuracoes.R')
 
 source('C:/local_R/projeto_karliane/scripts_comuns/funcoes.R')
 # source('~/R/karliane/projeto_karliane/nayves_bayes/funcoes.R')
-for (t in 1:2) { #1 = taxa 0,9 2 = taxa 0,95
-  for(c in 1:4){  # 1 = NB, 2 = AD 3 = ripper 4 = IBK
+for (t in 1:1) { #1 = taxa 0,9 2 = taxa 0,95
+  for(c in 1:1){  # 1 = NB, 2 = AD 3 = ripper 4 = IBK
     it_g <-c() 
     bd_g <-c()
     thrConf_g<-c()
@@ -49,7 +49,7 @@ for (t in 1:2) { #1 = taxa 0,9 2 = taxa 0,95
     
     bd <- c()
     tx <- c()
-    for(i in 2:2){  # bases de dados
+    for(i in 4:4){  # bases de dados
       for(j in 1:5){ # taxas  #base 1 - IRIS 5% NB N?O FUNCIONA - da erro
         taxa <- j*5
         source('C:/local_R/projeto_karliane/scripts_comuns/carrega_dados.R')
