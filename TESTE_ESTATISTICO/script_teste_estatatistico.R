8bc8db682bad4de62021ad86dd0f864a61b0986a
karliane Vale <karlianev@hotmail.com>
2018-02-22 15:19:54 -0300

    inclusao do teste estatistico

--- TESTE_ESTATISTICO/script_teste_estatatistico.R
+++ TESTE_ESTATISTICO/script_teste_estatatistico.R
@@ -4,8 +4,16 @@ library("PMCMR")
 setwd("C:\\local_R\\projeto_karliane\\TESTE_ESTATISTICO")
 #atribui os dados a uma matriz
 dados <- as.matrix(read.csv("T5.CSV"))
+
+
 #roda o teste
 friedman.test(dados)
-
-a <- pairwiseSignTest(data = dados, method="fdr")
+#roda pareado
 posthoc.friedman.conover.test(dados)
+
+
+
+#outras opções de teste pareado
+wilcox.test(dados)
+posthoc.durbin.test(dados)
+posthoc.friedman.nemenyi.test(dados)
