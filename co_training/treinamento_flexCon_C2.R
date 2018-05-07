5bae506457766bbfc435d716c3388adec2f7ce2e
karliane Vale <karlianev@hotmail.com>
2018-05-07 06:59:14 -0300

    inclusao da funcao cotrainflexcon-c1 e criacao dos scripts de treinamento q chamam essa funcao

---
+++ co_training/treinamento_flexCon_C2.R
@@ -0,0 +1,68 @@
+#chamando a funcao selfTrain adaptada
+print("Iniciando Treinamento")
+
+if(c==1){ #NAIVE BAYES
+  stdNB <- naiveBayes(as.formula(paste(classe,'~', '.')),base_rotulados_ini)
+  matriz_confusao_supervisionado <- table(predict(stdNB,base_rotulados_ini,type='class'),base_rotulados_ini$class)
+  acc_sup_3 <- ((sum(diag(matriz_confusao_supervisionado)) / sum(matriz_confusao_supervisionado)) * 100)
+  
+  if (t==1){ #TAXA INICIAL 0.9
+    #chamada da funcao que implementa o metodo flexCon_C2 usando naive
+    CT<- coTrainFlexCon_C2(as.formula(paste(classe,'~', '.')), base_treino_self_training,learner("naiveBayes", list(4)),'func',0.9,100,1,TRUE,qtd_exem_menor_classe, limiar = acc_sup_3, stdNB)
+  }else if (t==2){ #TAXA INICIAL 0.95
+    CT<- coTrainFlexCon_C2(as.formula(paste(classe,'~', '.')), base_treino_self_training,learner("naiveBayes", list(4)),'func',0.95,100,1,TRUE,qtd_exem_menor_classe, limiar = acc_sup_3, stdNB)
+  }
+}
+if(c==2){ #ARVORE DE DECISAO
+  stdTree <- rpartXse(as.formula(paste(classe,'~', '.')),base_rotulados_ini,se=0.5)
+  matriz_confusao_supervisionado <- table(predict(stdTree,base_rotulados_ini,type='class'),base_rotulados_ini$class)
+  acc_sup_3 <- ((sum(diag(matriz_confusao_supervisionado)) / sum(matriz_confusao_supervisionado)) * 100)
+  
+  if (t==1){ #TAXA INICIAL 0.9
+    #chamada da funcao que implementa o metodo modificado usando arvore de decis?o
+    CT<- coTrainFlexCon_C2(as.formula(paste(classe,'~', '.')), base_treino_self_training,learner('rpartXse',list(se=0.5)),'f',0.9,100,1,TRUE,qtd_exem_menor_classe, limiar = acc_sup_3, stdNB)
+  }else if (t==2){ #TAXA INICIAL 0.95
+    CT<- coTrainFlexCon_C2(as.formula(paste(classe,'~', '.')), base_treino_self_training,learner('rpartXse',list(se=0.5)),'f',0.95,100,1,TRUE,qtd_exem_menor_classe, limiar = acc_sup_3, stdNB)
+  }  
+}
+if(c==3){ #RIPPER
+  stdJRip <- JRip(as.formula(paste(classe,'~', '.')),base_rotulados_ini)
+  matriz_confusao_supervisionado <- table(predict(stdJRip,base_rotulados_ini,type='class'),base_rotulados_ini$class)
+  acc_sup_3 <- ((sum(diag(matriz_confusao_supervisionado)) / sum(matriz_confusao_supervisionado)) * 100)
+  
+  if (t==1){
+    CT<- coTrainFlexCon_C2(as.formula(paste(classe,'~', '.')), base_treino_self_training,learner('JRip',list()),'f2',0.9,100,1,TRUE,qtd_exem_menor_classe, limiar = acc_sup_3, stdNB)
+  }else if (t==2){
+    CT<- coTrainFlexCon_C2(as.formula(paste(classe,'~', '.')), base_treino_self_training,learner('JRip',list()),'f2',0.95,100,1,TRUE,qtd_exem_menor_classe, limiar = acc_sup_3, stdNB)
+  }  
+}
+if(c==4){ #IBK
+  stdIBK <- IBk(as.formula(paste(classe,'~', '.')),base_rotulados_ini, control = Weka_control(K=15, X=TRUE))
+  matriz_confusao_supervisionado <- table(predict(stdIBK,base_rotulados_ini,type='class'),base_rotulados_ini$class)
+  acc_sup_3 <- ((sum(diag(matriz_confusao_supervisionado)) / sum(matriz_confusao_supervisionado)) * 100)
+  
+  if (t==1){
+    CT<- coTrainFlexCon_C2(as.formula(paste(classe,'~', '.')), base_treino_self_training,learner('IBk',list(control = Weka_control(K=15, X=TRUE))),'f2',0.9,100,1,TRUE,qtd_exem_menor_classe, limiar = acc_sup_3, stdNB)
+  }else if (t==2){
+    CT<- coTrainFlexCon_C2(as.formula(paste(classe,'~', '.')), base_treino_self_training,learner('IBk',list(control = Weka_control(K=15, X=TRUE))),'f2',0.95,100,1,TRUE,qtd_exem_menor_classe, limiar = acc_sup_3, stdNB)
+  }  
+}
+
+matriz_confusao_v1<-table(predict(CT[[1]], base_teste, type = 'class'), base_teste$class)
+matriz_confusao_v2<-table(predict(CT[[2]], base_teste, type = 'class'), base_teste$class)
+
+#CALCULANDO A ACURACIA (AT? LINHA 109)
+acc_o_v1<-((sum(diag(matriz_confusao_v1)) / sum(matriz_confusao_v1)) * 100)
+acc_o_v2<-((sum(diag(matriz_confusao_v2)) / sum(matriz_confusao_v2)) * 100)
+acc_o <- (acc_o_v1 + acc_o_v2)/2
+acc_g_o <- c(acc_g_o, acc_o)
+
+
+#ARMAZENANDO EM UMA VARIAVEL O NOME DA BASE E O % DE EXEMPLOS ROTULADOS INICIALMENTE
+bd <- c(bd, bd_nome)
+tx <- c(tx, taxa)
+metodo <- c(metodo, num_metodo)
+
+#imprimindo a acuracia na tela 
+cat("\n Acerto global original (%) =", acc_o)
+cat("\n FIM") #, '\t base de dados ', i, '\n', 'total rotulados: ', total_rotulados, '\n')
