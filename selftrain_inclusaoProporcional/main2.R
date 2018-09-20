45bb9d241f168c3863a66b84b99e2061f62ebab4
cainanalves <cainan.teixeira10@hotmail.com>
2018-09-20 14:45:20 -0300

    Copia de arquivos para divisao de processos

---
+++ selftrain_inclusaoProporcional/main2.R
@@ -0,0 +1,66 @@
+# Função chega o SO utilizado e seta o diretório
+# This function check the OS and change work directory
+setWorkspace <- function() {
+  mySystem <- Sys.info()
+  if(mySystem[[1]] == "Linux"){
+    setwd("~/R/karliane/projeto_karliane/selftrain_inclusaoProporcional/")
+  }else{
+    setwd("C:\\local_R\\projeto_karliane\\selftrain_inclusaoProporcional")
+  }
+}
+
+setWorkspace()
+source('functions2.R')
+medias_c1_s <- cleanVector(medias_c1_s)
+medias_c1_v <- cleanVector(medias_c1_v)
+medias_c2 <- cleanVector(medias_c2)
+# Carregando o script com as funções
+# Loading functions script
+
+installNeedPacks()
+source('crossValidation.R')
+
+initGlobalVariables()
+defines()
+
+newBase <- function(base_rotulada, ids_treino_rot){
+    base_rotulada[- ids_treino_rot, "class"] <- NA
+    return (base_rotulada)
+}
+
+for (cr in change_rate) {
+  for (cl in 1:length(classifiers)) {
+    for(i in 0:30) {
+      source('databases.R')
+      k_NN <- attKValue(base_original)
+      qtd_exem_por_classe <- ddply(base_original, ~class, summarise, number_of_distinct_orders = length(class))
+      qtd_exem_menor_classe <- trunc(min(qtd_exem_por_classe$number_of_distinct_orders) * 0.1)
+      folds <- crossValidation(base_original, base_original$class)
+      for(j in 1:5) {
+        taxa <- j * 5
+        acc_c1_s <- cleanVector(acc_c1_s)
+        acc_c1_v <- cleanVector(acc_c1_v)
+        acc_c2 <- cleanVector(acc_c2)
+        for (fold in 1:length(folds)) {
+          base_teste <- base_original[folds[[fold]], ]
+          base <- base_original[- folds[[fold]], ]
+          treinamento <<- base_rotulada_treino <- base
+          #sorteando os exemplos que ficarão rotulados inicialmente
+          cat("\nCR:", cr, "   CL:", classifiers[cl], "   BD:", i, "   TX:", j, "   FOLD:", fold)
+          H2 <- holdout(base_rotulada_treino$class, ratio = (taxa / 100), mode = "stratified")
+          ids_treino_rot <- H2$tr
+          base <- newBase(base_rotulada_treino, ids_treino_rot)
+          base_rotulados_ini <- base_rotulada_treino[ids_treino_rot, ]
+          source('training.R')
+        }
+        medias_c1_s <- appendVectors(medias_c1_s, acc_c1_s)
+        medias_c1_v <- appendVectors(medias_c1_v, acc_c1_v)
+        medias_c2 <- appendVectors(medias_c2, acc_c2)
+      }
+    }
+    output_archive(cr, as.character(classifiers[cl]), medias_c1_s, medias_c1_v, medias_c2)
+    medias_c1_s <- cleanVector(medias_c1_s)
+    medias_c1_v <- cleanVector(medias_c1_v)
+    medias_c2 <- cleanVector(medias_c2)
+  }
+}
--- selftrain_inclusaoProporcional/training.R
+++ selftrain_inclusaoProporcional/training.R
@@ -27,24 +27,24 @@ n <- getLength(base_teste$class)
 # cat("\n Acerto global flexcon-c2    (%) =", partial_acc_c2)
 
 
-# 
-# # FlexCon-C1S (s)
-# flex_con_c1_s <- flexConC(my_learner, my_function, qtd_exem_menor_classe, limiar, "1", stratified = TRUE)
-# matrix_c1_s <- confusionMatrix(flex_con_c1_s)
-# partial_acc_c1_s <- getAcc(matrix_c1_s, n)
-# acc_c1_s <- appendVectors(acc_c1_s, partial_acc_c1_s)
-# cat("\n Acerto global flexcon-c1S(s) (%) =", partial_acc_c1_s)
-# 
-# # FlexCon-C1S (v)
-# flex_con_c1_v <- flexConC(my_learner, my_function, qtd_exem_menor_classe, limiar, "2", stratified = TRUE)
-# matrix_c1_v <- confusionMatrix(flex_con_c1_v)
-# partial_acc_c1_v <- getAcc(matrix_c1_v, n)
-# acc_c1_v <- appendVectors(acc_c1_v, partial_acc_c1_v)
-# cat("\n Acerto global flexcon-c1S(v) (%) =", partial_acc_c1_v)
 
-# FlexCon-C2S
-flex_con_c2 <- flexConC(my_learner, my_function, qtd_exem_menor_classe, limiar, "3", stratified = TRUE)
-matrix_c2 <- confusionMatrix(flex_con_c2)
-partial_acc_c2 <- getAcc(matrix_c2, n)
-acc_c2 <- appendVectors(acc_c2, partial_acc_c2)
-cat("\n Acerto global flexcon-c2S    (%) =", partial_acc_c2)
+# FlexCon-C1S (s)
+flex_con_c1_s <- flexConC(my_learner, my_function, qtd_exem_menor_classe, limiar, "1", stratified = TRUE)
+matrix_c1_s <- confusionMatrix(flex_con_c1_s)
+partial_acc_c1_s <- getAcc(matrix_c1_s, n)
+acc_c1_s <- appendVectors(acc_c1_s, partial_acc_c1_s)
+cat("\n Acerto global flexcon-c1S(s) (%) =", partial_acc_c1_s)
+
+# FlexCon-C1S (v)
+flex_con_c1_v <- flexConC(my_learner, my_function, qtd_exem_menor_classe, limiar, "2", stratified = TRUE)
+matrix_c1_v <- confusionMatrix(flex_con_c1_v)
+partial_acc_c1_v <- getAcc(matrix_c1_v, n)
+acc_c1_v <- appendVectors(acc_c1_v, partial_acc_c1_v)
+cat("\n Acerto global flexcon-c1S(v) (%) =", partial_acc_c1_v)
+
+# # FlexCon-C2S
+# flex_con_c2 <- flexConC(my_learner, my_function, qtd_exem_menor_classe, limiar, "3", stratified = TRUE)
+# matrix_c2 <- confusionMatrix(flex_con_c2)
+# partial_acc_c2 <- getAcc(matrix_c2, n)
+# acc_c2 <- appendVectors(acc_c2, partial_acc_c2)
+# cat("\n Acerto global flexcon-c2S    (%) =", partial_acc_c2)
