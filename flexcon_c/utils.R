8a5b8809ab673e463ce298e2efa442a912b8c73c
ArthurGorgonio <gorgonioarthur@gmail.com>
2018-10-05 15:32:26 -0300

    Funções uteis

---
+++ flexcon_c/utils.R
@@ -0,0 +1,32 @@
+#' @description Provide a way to paste 2 or more words.
+#'
+#' @param vec the words to be pasted in the order.
+#'
+#' @return a string with the vec words collapsed in _.
+join <- function(vec, collapse = "_") {
+  return (paste(vec[1:length(vec)], collapse = collapse))
+}
+
+#' @description Provide a way to read files and put names in the columns.
+#'
+#' @param file The file must be read.
+#'
+#' @return The content of the file.
+#'
+readFile <- function(file) {
+  return (read.csv(file, header = FALSE, col.names = c("V1", "V2", "V3", "V4",
+                                                       "V5")))
+}
+
+#' @description Write in the output file the content.
+#'
+#' @usage write_archive (title, content, append = TRUE)
+#'
+#' @param title the title of the file.
+#' @param content the content of the file.
+#' @param append the method to write in the archive.
+#'
+writeArchive <- function(title, content, append = TRUE) {
+  write.table(content, title, row.names = FALSE, append = append, sep = ",",
+              col.names = FALSE)
+}
