1c3d5f72e00a1143e16ebbfb3be856be1e064469
ArthurGorgonio <gorgonioarthur@gmail.com>
2018-10-08 11:03:31 -0300

    Melhorando visualização do código e comentando

--- flexcon_c/utils.R
+++ flexcon_c/utils.R
@@ -1,8 +1,8 @@
-#' @description Install packages if it not installed and load them.
+#' @description Install packages if it was not installed and load them.
 #'
 installNeedPacks <- function() {
-  packages <- c("ssc", "plyr", "DMwR", "DMwR2", "RWeka",
-                "rminer", "e1071", "ggplot2", "stats")
+  packages <- c("ssc", "plyr", "DMwR", "DMwR2", "RWeka", "rminer", "e1071",
+                "ggplot2", "stats")
   for (pack in packages) {
     if (!require(pack, character.only = TRUE)) {
       install.packages(pack)
